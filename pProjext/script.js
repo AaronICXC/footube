5a356aa15fccb01df1ffd59e689fc9d4875fb2c2
Aaron R <162951106+AaronYUH@users.noreply.github.com>
2024-04-01 09:25:01 -0500

    Add files via upload

---
+++ pProjext/script.js
@@ -0,0 +1,15 @@
+function homeClick() {
+	document.location.href = "index.html";
+}
+
+function premiumClick() {
+	document.location.href = "premium.html";
+}
+
+function kidsClick() {
+	document.location.href = "kids.html";
+}
+
+function aboutClick() {
+	document.location.href = "about.html";
+}
